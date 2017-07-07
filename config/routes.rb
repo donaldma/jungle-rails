749eff5c69b688dc56140d25129a2429f50bc3e5
donaldma <donald.ma@hotmail.ca>
2017-07-07 20:44:31 +0000

    add email receipt feature

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create, :destroy]
   end
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
