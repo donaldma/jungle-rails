a922d29bac1aa988e6d3a2d302bba2f78fa54de1
donaldma <donald.ma@hotmail.ca>
2017-07-12 06:10:44 +0000

    add feature spec add to cart / modified feature spec product details

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,16 +16,18 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They see product details" do
+  scenario "They see cart +1 when add to cart is clicked" do
     # ACT
     visit root_path
 
-    first('article.product').click_link('Details')
+    first('article.product').click_link('Add')
 
     # DEBUG
     save_screenshot
 
     # VERIFY
-    expect(page).to have_css '.product-detail'
+    within ('#navbar') do
+      expect(page).to have_content 'My Cart (1)'
+    end
   end
 end
